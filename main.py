5e7bdbf7629e4434004630390b0d897ccb048d08
ASH-tg <142460976+ASH-tg@users.noreply.github.com>
2023-08-17 07:03:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from plyer import gps
+from plyer import permission
 
-class GPSApp(App):
+class LocationApp(App):
     def build(self):
-        self.label = Label(text="Waiting for GPS data...")
-        self.gps = gps
-        self.gps.configure(on_location=self.update_location)
-        self.gps.start(minTime=1000, minDistance=0)
-        return self.label
+        layout = BoxLayout(orientation='vertical')
+        self.label = Label(text="Waiting for location...")
+        layout.add_widget(self.label)
+        self.request_location_permission()
+        return layout
 
-    def update_location(self, **kwargs):
-        latitude = kwargs.get('lat')
-        longitude = kwargs.get('lon')
-        accuracy = kwargs.get('accuracy')
-        self.label.text = f"Latitude: {latitude}\nLongitude: {longitude}\nAccuracy: {accuracy} meters"
+    def request_location_permission(self):
+        if permission.check_permission('location'):
+            self.start_location_tracking()
+        else:
+            permission.request_permission('location', self.permission_callback)
 
-    def on_stop(self):
-        self.gps.stop()
+    def permission_callback(self, permission, granted):
+        if granted:
+            self.start_location_tracking()
+        else:
+            self.label.text = "Location permission denied."
 
-if __name__ == "__main__":
-    GPSApp().run()
+    def start_location_tracking(self):
+        gps.configure(on_location=self.on_location)
+        gps.start(minTime=1000, minDistance=1)
+
+    def on_location(self, **kwargs):
+        self.label.text = "Latitude: {}\nLongitude: {}".format(kwargs['lat'], kwargs['lon'])
+
+if __name__ == '__main__':
+    LocationApp().run()
